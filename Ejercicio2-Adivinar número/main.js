8d4c79ec48a1b783eb736669011a026e4d0907ed
Álvaro Sierra <asierraperez@danielcastelao.org>
2021-11-03 11:51:32 +0100

    v0

--- Ejercicio2-Adivinar número/main.js
+++ Ejercicio2-Adivinar número/main.js
@@ -3,23 +3,7 @@ function main() {
     do {
         var n_random = random(n_random)
         comparacion(n_random)
-       /* for (let index = 0; index < 5; index++) {
-            var n_introducido = entrada()
-            if (n_introducido == n_random) {
-                alert("Enhorabuena!!,\nAcertaste el numero\nX = "+n_random)
-                break
-            }
-            if (n_introducido < n_random) {
-                alert("Fallaste,\n" + n_introducido + " < X\nTe quedan " + (5 - (index + 1)) + " intentos")
-            } else if (n_introducido > n_random) {
-                alert("Fallaste,\n" + n_introducido + " > X\nTe quedan " + (5 - (index + 1)) + " intentos")
-            }
-
-        }*/
         confirmar=confirmacion()
-
     } while (confirmar == true );
-
-
 }
 main()
