d2e18d0e429281466306002ce42e42843b2bc165
Álvaro Sierra <asierraperez@danielcastelao.org>
2021-11-03 13:37:02 +0100

    v0

--- Ejercicio3-Perfiles/entrada.js
+++ Ejercicio3-Perfiles/entrada.js
@@ -1,18 +1,18 @@
 function entrada(user,usuario) {
-    //user.nombre = prompt("Introduce tu nombre")
-    user.nombre ="alvaro"
+    user.nombre = prompt("Introduce tu nombre")
+    //user.nombre ="alvaro"
     usuario[0] = user.nombre
-    //user.apellidos = prompt("Introduce tu apellidos")
-    user.apellidos ="Sierra"
+    user.apellidos = prompt("Introduce tu apellidos")
+    //user.apellidos ="Sierra"
     usuario[1] = user.apellidos
-    //user.direccion = prompt("Introduce tu direccion")
-    user.direccion ="Calle falsa"
+    user.direccion = prompt("Introduce tu direccion")
+    //user.direccion ="Calle falsa"
     usuario[2] = user.direccion
-    //user.tlfno = prompt("Introduce tu numero de telefono")
-    user.tlfno ="123456"
+    user.tlfno = prompt("Introduce tu numero de telefono")
+    //user.tlfno ="123456"
     usuario[3] = user.tlfno
-    //user.email = prompt("Introduce tu direccion de email")
-    user.email ="a@jiop.es"
+    user.email = prompt("Introduce tu direccion de email")
+    //user.email ="a@jiop.es"
     usuario[4] = user.email
     console.table(user)
 }
