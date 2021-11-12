56a30c2ccf843c009f6cf533f89a862241171855
Álvaro Sierra <asierraperez@danielcastelao.org>
2021-11-12 09:56:38 +0100

    v1

--- Ejercicio1-Cajero automático/conteo.js
+++ Ejercicio1-Cajero automático/conteo.js
@@ -1,15 +1,18 @@
-function conteo(dinero,billetes,contar,tabla){
+function conteo(dinero, tabla) {
+    var aux_dinero = dinero
+    var aux_tabla = tabla
+    var billetes = [0, 0, 0, 0, 0, 0]
+    var contar = [200, 100, 50, 20, 10, 5]
     for (let index = 0; index < 6; index++) {
-        while (dinero >= contar[index]) {
-            dinero = dinero - contar[index]
+        while (aux_dinero >= contar[index]) {
+            aux_dinero = aux_dinero - contar[index]
             billetes[index]++
             console.log(billetes[index])
         }
         if (billetes[index] > 0) {
-            tabla = tabla + (billetes[index] + " billete de " + contar[index] + " euros\n")
-            console.log(tabla)
+            aux_tabla = aux_tabla + (billetes[index] + " billete de " + contar[index] + " euros\n")
+            console.log(aux_tabla)
         }
-
     }
-    return tabla
+    return aux_tabla
 }
--- Ejercicio1-Cajero automático/main.js
+++ Ejercicio1-Cajero automático/main.js
@@ -1,14 +1,12 @@
 function main() {
     var dinero
     var valido
-    var billetes = [0,0,0,0,0,0]
     var tabla = ""
-    var contar = [200, 100, 50, 20, 10, 5]
     do {
         dinero = entrada()
         valido = comprobar(dinero)
     } while (valido != 0);
-    tabla=conteo(dinero,billetes,contar,tabla)
+    tabla = conteo(dinero, tabla)
     salida(tabla)
 }
 main()
